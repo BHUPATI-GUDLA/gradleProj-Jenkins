76249ed2c48fe80207432ab1a689a771db091575
Bhupati Gudla <bhupatigudla00@gmail.com>
2021-08-06 10:37:28 +0530

    adding calculator

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+
+	float add(float a,float b) {
+		return a+b;
+	}
+	
+	float sub(float a,float b) {
+		return a-b;
+	}
+	
+	int multiplication(int a,int b,int c) {
+		return a*b*c;
+	}
+	
+}
